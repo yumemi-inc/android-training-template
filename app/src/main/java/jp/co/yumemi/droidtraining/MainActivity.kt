b093a55aa48ce10c322e16247b9655b052654510
k_senda <k_senda@yumemi.co.jp>
2023-10-06 17:04:58 +0900

    add: template/compose

--- app/src/main/java/jp/co/yumemi/droidtraining/MainActivity.kt
+++ app/src/main/java/jp/co/yumemi/droidtraining/MainActivity.kt
@@ -1,12 +1,55 @@
 package jp.co.yumemi.droidtraining
 
 import android.os.Bundle
-import androidx.appcompat.app.AppCompatActivity
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import jp.co.yumemi.droidtraining.ui.theme.YumemiTheme
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : ComponentActivity() {
 
+    @OptIn(ExperimentalMaterial3Api::class)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContent {
+            YumemiTheme {
+                Scaffold(
+                    topBar = {
+                        TopAppBar(
+                            title = {
+                                Text(text = stringResource(id = R.string.app_name))
+                            },
+                        )
+                    },
+                ) { innerPadding ->
+                    Box(
+                        contentAlignment = Alignment.Center,
+                        modifier = Modifier
+                            .fillMaxSize()
+                            .padding(innerPadding),
+                    ) {
+                        Greeting()
+                    }
+                }
+            }
+        }
     }
+}
+
+@Composable
+fun Greeting(modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello World!",
+        modifier = modifier
+    )
 }
